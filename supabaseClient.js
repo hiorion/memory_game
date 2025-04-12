86ff09e21b0bdb9167f955bf8709d09d64af6641
Henry Orion <hen.orion@gmail.com>
2025-04-12 02:48:20 -0300

    adicao do client.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,3 +1,3 @@
 // supabaseClient.js
-export const SUPABASE_URL = "https://apxdzmbhnwvybptilekq.supabase.co";
-export const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // sua chave pública completa
+export const SUPABASE_URL = "https://mmsictuwoiyjhporyqpu.supabase.co";
+export const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tc2ljdHV3b2l5amhwb3J5cXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0MzU0MTgsImV4cCI6MjA2MDAxMTQxOH0.pl9MMim624frRJlR_2B_G20N2R99aux8gSFqpsVynqw"; // sua chave pública completa
